4a5dc2ad1978e2bd0e490707d408049b71b9a7c9
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2023-10-26 16:16:04 +0300

    js

--- flavokrkkk1.1/main.js
+++ flavokrkkk1.1/main.js
@@ -1,6 +1,14 @@
   const btn = document.querySelector('.menu-btn');
   const nav = document.querySelector('.nav');
+  const btntwo = document.querySelector('.headline-btn');
+ 
 
+
+    btntwo.addEventListener('click', () => {
+      btntwo.style.backgroundColor = 'white'; 
+      btntwo.style.color = 'black';
+      document.documentElement.scrollTo(0, 1500);
+    })
 
   btn.addEventListener('click',()=>{
     nav.classList.toggle('menu-open');
