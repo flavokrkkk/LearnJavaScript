15b06d61431f3aa08af509f5111ed08d459fb492
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2023-10-26 17:39:46 +0300

    Working to JS

--- flavokrkkk1.1/main.js
+++ flavokrkkk1.1/main.js
@@ -1,6 +1,7 @@
   const btn = document.querySelector('.menu-btn');
   const nav = document.querySelector('.nav');
   const btntwo = document.querySelector('.headline-btn');
+  const input = document.querySelectorAll('.contacts-input');
  
 
 
@@ -14,3 +15,17 @@
     nav.classList.toggle('menu-open');
 
   });
+
+  //  input.forEach(function(el){
+  //   el.addEventListener('click', function(event){ // инпут на всякий случай
+  //     console.log(this);
+  //     this.style.color = '#fff';
+  //   })
+  // }) 
+
+  input.forEach(function(el){
+    el.addEventListener('click', function(event){ // инпут на всякий случай
+      console.log(this);
+      this.style.color = '#fff';
+    })
+  }) 
